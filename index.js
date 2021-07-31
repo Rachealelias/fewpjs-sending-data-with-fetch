5c6268683b7a2dce1954350a613d25d25c812d8b
Rachealelias <rachealsamuel708@gmail.com>
2021-07-31 11:16:45 -0400

    created fetch post

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName,userEmail){
+   return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers:{
+            "Content-Type": "applicstion/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Racheal",
+            email: "racheal@gmail.com"
+        })
+        })
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(object){
+        console.log(object);
+    })
+    .catch(function(error){
+        alert("oops! Please try again later");
+        console.log(error.message);
+    })
+}
